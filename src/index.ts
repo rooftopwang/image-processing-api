aff173c0e92f62bc9cc4bff10e5de528ea4fe6f8
wolfgang.w <wolfgangwang@hotmail.ca>
2021-07-09 17:06:02 +0800

    config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-// import express from "express";
-const a = "a";
+import express from "express";
 
-export default a;
+const app = express();
+const PORT = 3000; 
+
+app.get("/test", (req, res) => {
+    console.log(req.body);
+    res.send("200, OK. ");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`app listening at http://localhost:${PORT}`)
+})
+export default app;
